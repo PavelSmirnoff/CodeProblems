1b592d5804646981ff06e17dd9eb5f8c45d3d9b0
Pavel Smirnov <smirnov@0x55.ru>
2021-04-28 11:41:48 +0300

    Add Maven to project

---
+++ src/main/java/dev/smirnoff/TwoSum/TwoSum.java
@@ -0,0 +1,18 @@
+package dev.smirnoff.TwoSum;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 28.04.2021
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
