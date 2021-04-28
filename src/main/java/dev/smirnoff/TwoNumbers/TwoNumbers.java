3175935936deb68f69a7ac771c02126223b815c1
Pavel Smirnov <smirnov@0x55.ru>
2021-04-28 16:26:51 +0300

    add task TwoNumbers

---
+++ src/main/java/dev/smirnoff/TwoNumbers/TwoNumbers.java
@@ -0,0 +1,20 @@
+package dev.smirnoff.TwoNumbers;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 28.04.2021
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class TwoNumbers {
+}
