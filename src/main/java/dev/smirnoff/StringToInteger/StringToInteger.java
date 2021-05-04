2ae723b79c8fa69d6d5a961ed535d926bdbbfd7d
Pavel Smirnov <smirnov@0x55.ru>
2021-05-04 18:47:01 +0300

    String to Integer (atoi)

--- src/main/java/dev/smirnoff/StringToInteger/StringToInteger.java
+++ src/main/java/dev/smirnoff/StringToInteger/StringToInteger.java
@@ -9,8 +9,79 @@ package dev.smirnoff.StringToInteger;
  */
 public class StringToInteger {
     public int myAtoi(String s) {
-        if (s.length() == 0) return 0;
+//        if (s.length() == 0) return 0;
+//        s = s.trim();
+//        int sign = 1;
+//        long l = 0;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//
+//            if ((c == '-') && i == 0) {
+//                sign = -1;
+//                continue;
+//            }
+//            if ((c == '+') && i == 0) {
+//                continue;
+//            }
+//
+//            if (c >= '0' && c <= '9') {
+//                l = (long) (l + (c - '0') * Math.pow(10, i));
+//                if (l > Integer.MAX_VALUE) {
+//                    l = Integer.MAX_VALUE;
+//                    break;
+//                }
+//            } else break;
+//        }
+//        System.out.println(l);
+//        return (int) (sign * l);
 
-        return 0;
+//        StringBuilder sb = new StringBuilder();
+//        if (s.length() == 0) return 0;
+//        boolean isNum = false;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if (c == ' ') if (sb.length() == 0) continue;
+//            else break;
+//            if (c == '-' || c == '+') if (sb.length() == 0) {
+//                sb.append(c);
+//                continue;
+//            } else if (sb.length() > 0) return 0;
+//            if (c >= '0' && c <= '9') {
+//                sb.append(c);
+//                isNum = true;
+//            } else break;
+//        }
+//        if (!isNum) return 0;
+//        if (sb.length() > 16) sb.setLength(16);
+//        long l = Long.parseLong(sb.toString());
+//
+//        return (l > Integer.MAX_VALUE) ? Integer.MAX_VALUE : (int) Math.max(l, Integer.MIN_VALUE);
+
+        StringBuilder sb = new StringBuilder();
+        int sign = 1;
+        s = s.trim();
+
+        if (s.length() == 0) return 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c == '-' || c == '+') && i == 0) {
+                if (c == '-') sign = -1;
+                continue;
+            }
+            if (c >= '0' && c <= '9') {
+                sb.append(c);
+            } else break;
+        }
+        s = sb.reverse().toString();
+        long result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = (long) (result + (s.charAt(i) - '0') * Math.pow(10, i));
+            if (result > Integer.MAX_VALUE) {
+                return (sign == 1 ) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+        }
+        return (int) (sign * result);
     }
 }
--- src/test/java/dev/smirnoff/StringToInteger/StringToIntegerTest.java
+++ src/test/java/dev/smirnoff/StringToInteger/StringToIntegerTest.java
@@ -1,10 +1,9 @@
 package dev.smirnoff.StringToInteger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This write description created class
@@ -19,23 +18,25 @@ class StringToIntegerTest {
 
     @Test
     @DisplayName("Test 1")
-    public void Test1(){
+    public void Test1() {
         assertEquals(42, sti.myAtoi("42"));
-        fail("Test 1 failed;");
     }
+
     @Test
     @DisplayName("Test 2")
-    public void Test2(){
+    public void Test2() {
         assertEquals(-42, sti.myAtoi("   -42"));
     }
+
     @Test
     @DisplayName("Test 3")
-    public void Test3(){
+    public void Test3() {
         assertEquals(4193, sti.myAtoi("4193 with words"));
     }
+
     @Test
     @DisplayName("Test 4")
-    public void Test4(){
+    public void Test4() {
         assertEquals(0, sti.myAtoi("words and 987"));
     }
 
@@ -43,6 +44,30 @@ class StringToIntegerTest {
     @DisplayName("Test 5")
     public void Test5() {
         assertEquals(-2147483648, sti.myAtoi("-91283472332"));
+    }
+
+    @Test
+    @DisplayName("Test 6")
+    public void Test6() {
+        assertEquals(0, sti.myAtoi("+-12"));
+    }
+
+    @Test
+    @DisplayName("Test 7")
+    public void Test7() {
+        assertEquals(0, sti.myAtoi("+"));
+    }
+
+    @Test
+    @DisplayName("Test 8")
+    public void Test8() {
+        assertEquals(2147483647, sti.myAtoi("20000000000000000000"));
+    }
+
+    @Test
+    @DisplayName("Test 9")
+    public void Test9() {
+        assertEquals(12345678, sti.myAtoi("  0000000000012345678"));
     }
 
 }
