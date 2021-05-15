e85d6a6d74707192d0241aca4521aeaa93e63d53
Pavel Smirnov <smirnov@0x55.ru>
2021-05-15 17:29:37 +0300

    MoveZeroes #fix01

--- src/main/java/dev/smirnoff/MoveZeroes/MoveZeroes.java
+++ src/main/java/dev/smirnoff/MoveZeroes/MoveZeroes.java
@@ -1,12 +1,8 @@
 package dev.smirnoff.MoveZeroes;
 
 
-import java.util.Arrays;
-
 public class MoveZeroes {
     public int[] moveZeroes(int[] nums) {
-
-        //Arrays.sort(nums);
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
