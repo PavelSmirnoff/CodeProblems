82a5d66823a07958a93e1e18b0a2f8fef3dc30d4
Pavel Smirnov <smirnov@0x55.ru>
2021-08-13 07:59:21 +0300

    add JUnit test

--- src/test/java/dev/smirnoff/Easy/ValidParentheses/ValidParentheses.java
+++ src/test/java/dev/smirnoff/Easy/ValidParentheses/ValidParentheses.java
@@ -1,5 +1,7 @@
 package dev.smirnoff.Easy.ValidParentheses;
 
 public class ValidParentheses {
-
+    public boolean isValid(String s) {
+        return true;
+    }
 }
---
+++ src/test/java/dev/smirnoff/Easy/ValidParentheses/ValidParenthesesTest.java
@@ -0,0 +1,28 @@
+package dev.smirnoff.Easy.ValidParentheses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidParenthesesTest {
+    ValidParentheses v = new ValidParentheses();
+
+    @Test
+    public void Test1(){
+        assertTrue(v.isValid("()"));
+    }
+    @Test
+    public void Test2(){
+        assertTrue(v.isValid("()[]{}"));
+    }
+    @Test
+    public void Test3(){
+        assertFalse(v.isValid("(]"));
+    }
+    @Test
+    public void Test4(){
+        assertFalse(v.isValid("([)]"));
+    }
+
+
+}
