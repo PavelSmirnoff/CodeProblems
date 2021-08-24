f82e291964c3220a771eed0d758b8fec2a3035cc
PavelSmirnoff <smirnov@0x55.ru>
2021-08-25 00:16:07 +0300

    Solved Jump Game

--- src/main/java/dev/smirnoff/Medium/JumpGame/JumpGame.java
+++ src/main/java/dev/smirnoff/Medium/JumpGame/JumpGame.java
@@ -2,12 +2,25 @@ package dev.smirnoff.Medium.JumpGame;
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
-        if (nums.length==1) return true;
-        return false;
+        if (nums.length == 1) return true;
+
+        int max_reach = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (max_reach < i) return false;
+            if (i + nums[i] > max_reach) max_reach = i + nums[i];
+        }
+        return true;
+        // return helper(0, nums);
     }
 
-    boolean helper(int iDx, int[] nums){
-
+    boolean helper(int iDx, int[] nums) {
+        int maxStep = iDx + nums[iDx];
+        if (maxStep >= nums.length - 1) return true;
+        System.out.println(nums[iDx] + " - " + nums.length);
+        for (int i = iDx; i < maxStep; ) {
+            if (nums[i] + iDx >= nums.length - 1) return true;
+            if (helper(++i, nums)) return true;
+        }
         return false;
     }
 }
--- src/test/java/dev/smirnoff/Medium/JumpGame/JumpGameTest.java
+++ src/test/java/dev/smirnoff/Medium/JumpGame/JumpGameTest.java
@@ -8,14 +8,17 @@ class JumpGameTest {
     JumpGame j = new JumpGame();
 
     @Test
-    public void Test1()
-    {
+    public void Test1() {
         assertTrue(j.canJump(new int[]{2,3,1,1,4}));
     }
 
     @Test
-    public void Test2()
-    {
+    public void Test2() {
         assertFalse(j.canJump(new int[]{3,2,1,0,4}));
+    }
+
+    @Test
+    public void Test3() {
+        assertTrue(j.canJump(new int[]{3,0,8,2,0,0,1}));
     }
 }
