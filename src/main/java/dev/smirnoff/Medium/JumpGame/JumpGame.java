8a21e7ae943e2b3304d3062d21f4a90279ad734f
PavelSmirnoff <smirnov@0x55.ru>
2021-08-24 23:01:16 +0300

    Start Jump Game

---
+++ src/main/java/dev/smirnoff/Medium/JumpGame/JumpGame.java
@@ -0,0 +1,11 @@
+package dev.smirnoff.Medium.JumpGame;
+
+/**
+ * This write description created class
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 24.08.2021
+ */
+public class JumpGame {
+}
